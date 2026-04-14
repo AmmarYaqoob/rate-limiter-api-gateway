5eab20fdd6fa9a2310e9b563fb74f0b6301d4c3f
ammar <ayaqoob@regtify.com>
2026-04-14 15:40:17 +0200

    updated redis uri

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,15 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("process.env.REDIS_HOST, process.env.REDIS_PORT");
+console.log(process.env.REDIS_HOST, process.env.REDIS_PORT);
 export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
 });
 
 redis.on("connect", () => {
-  console.log("Redis connected");
+    console.log("Redis connected");
 });
 
 redis.on("error", (err) => {
-  console.error("Redis error:", err.message);
+    console.error("Redis error:", err.message);
 });
